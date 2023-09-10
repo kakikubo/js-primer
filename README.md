33f3f1fa3e3acb9770a334b1d94d5bda4e0577f0
kakikubo <kakikubo@gmail.com>
2023-09-11 06:40:26 +0900

    strictモードに関しての説明を追加

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # js-primer
 
-元からformはせず、一旦個別に作成してみる
-
+元はこちら。 <https://github.com/asciidwango/js-primer>
+本家からforkはせず、一旦個別に作成してみる
 各ディレクトリにてサンプルのコードを記載していく事にする。
 
 ## 第1部 基本文法
--- chapter1/variable.js
+++ chapter1/variable.js
@@ -1,3 +1,8 @@
+"use strict";
+// このコードは strict mode で実行される
+
+// mistypedVaraible = 42; // ReferenceError: mistypedVaraible is not defined
+
 // 変数は大文字小文字を区別する
 const name = "azu";
 const NAME = "azu";
