34f192224cd5dd6552b24cd6a89a7da5c72fd0b2
kakikubo <kakikubo@gmail.com>
2023-10-04 06:53:24 +0900

    まとめ
    - 関数やブロックはスコープを持つ
    - スコープはネストできる
    - もっとも外側にはグローバルスコープがある
    - スコープチェーンは内側から外側のスコープへと順番に変数が定義されているか探す仕組みのこと
    - varキーワードでの変数宣言やfunctionでの関数宣言では巻き上げが発生する
    - クロージャーは静的スコープとメモリ管理の仕組みからなる関数が持つ性質

--- chapter18/function-scope.js
+++ chapter18/function-scope.js
@@ -99,3 +99,97 @@ console.log(Array); // => function Array() { [native code] }
   }
   run();
 }
+
+// メモリ管理の仕組み
+{
+  let x = "before text";
+  // 変数`x`に新しいデータを入れる
+  x = "after text";
+  // このとき"before text"というデータはどこからも参照されなくなる
+  // その後、ガベージコレクションによってメモリ上から開放される
+}
+
+// 関数の実行が終了した際に開放される場合
+{
+  function printX() {
+    const x = "X";
+    console.log(x);
+  }
+  printX(); // => "X"
+  // この時点で”X"を参照するものはなくなる→開放される
+}
+
+// 関数の実行が終了しても開放されない場合
+{
+  function createArray() {
+    const tempArray = [1, 2, 3];
+    return tempArray;
+  }
+  const array = createArray();
+  console.log(array); // => [1, 2, 3]
+  // 変数`array`が[1,2,3]という値を参照している→開放されない
+}
+
+// 改めてクロージャー
+{
+  const createCounter = () => {
+    let count = 0;
+    return function increment() {
+      // 変数`count`を参照し続けている
+      count = count + 1;
+      return count;
+    };
+  };
+  // countUpとnewCountUpはそれぞれ別のincrement関数(内側にあるのも別のcount変数)
+  const countUp = createCounter();
+  const newCountUp = createCounter();
+  // 参照している関数(オブジェクト)は別であるため===は一致しない
+  console.log(countUp === newCountUp); // false
+  // それぞれの状態も別となる
+  console.log(countUp()); // => 1
+  console.log(newCountUp()); // => 1
+}
+{
+  const createCounter = () => {
+    // 外のスコープから`privateCount`を直接参照できない
+    let privateCount = 0;
+    return () => {
+      privateCount++;
+      return `${privateCount}回目`;
+    };
+  };
+  const counter = createCounter();
+  console.log(counter()); // => "1回目"
+  console.log(counter()); // => "2回目"
+}
+// 高階関数(関数を返す関数)
+{
+  function greaterThan(n) {
+    return function (m) {
+      return m > n;
+    };
+  }
+  // 5より大きな値かを判定する関数を作成する
+  const greaterThan5 = greaterThan(5);
+  console.log(greaterThan5(4)); // => false
+  console.log(greaterThan5(5)); // => false
+  console.log(greaterThan5(6)); // => true
+}
+
+// 関数はオブジェクトである…という事を利用して、関数にプロパティをもたせて上記の
+// クロージャーと同じ事を実現できる(しかし、後述の※により推奨されない)
+{
+  function countUp() {
+    // countプロパティを参照して変更する
+    countUp.count = countUp.count + 1;
+    return countUp.count;
+  }
+  // 関数オブジェクトにプロパティとして値を代入する
+  countUp.count = 0;
+  // 呼び出すごとにcountが更新される
+  console.log(countUp()); // => 1
+  console.log(countUp()); // => 2
+  // 直接値を変更できてしまう。※クロージャーならこれはできない
+  countUp.count = 10;
+  console.log(countUp()); // => 11
+}
