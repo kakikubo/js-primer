65445e950c452244446fb693f379ec1f9ed543ab
kakikubo <kakikubo@gmail.com>
2023-10-08 06:13:58 +0900

    クラスのアクセッサプロパティの定義

--- chapter20/basic-class.js
+++ chapter20/basic-class.js
@@ -148,3 +148,29 @@
   // ベースオブジェクトはundefined
   console.log(method()); // => undefined
 }
+
+// クラスのアクセッサプロパティの定義
+{
+  class NumberWrapper {
+    constructor(value) {
+      this._value = value;
+    }
+    // _valueプロパティの値を返すgetter
+    get value() {
+      console.log("getter");
+      return this._value;
+    }
+    // _valueプロパティに値を代入するsetter
+    set value(newValue) {
+      console.log("setter");
+      this._value = newValue;
+    }
+  }
+  const numberWrapper = new NumberWrapper(1);
+  // "getter"とコンソールに表示される
+  console.log(numberWrapper.value); // => 1
+  // "setter"とコンソールに表示される
+  numberWrapper.value = 42;
+  // "getter"とコンソールに表示される
+  console.log(numberWrapper.value); // => 42
+}
