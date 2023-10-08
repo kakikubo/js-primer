7d941076b7920b4cb989ad379e3aea869043d453
kakikubo <kakikubo@gmail.com>
2023-10-08 16:11:04 +0900

    ビルトインエラー

--- chapter21/error-try-catch.js
+++ chapter21/error-try-catch.js
@@ -57,3 +57,38 @@
     console.log(error); // => "例外が投げられました"
   }
 }
+{
+  // ビルトインエラー(ReferenceError)
+  try {
+    // 存在しない変数を参照する
+    console.log(x);
+  } catch (error) {
+    console.log(error instanceof ReferenceError); // => true
+    console.log(error.name); // => "ReferenceError"
+    console.log(error.message); // エラーメッセージが表示される
+  }
+}
+{
+  // ビルトインエラー(SyntaxError)
+  try {
+    // eval関数は渡した文字列をJavaScriptとして実行する関数
+    // 正しくない構文をパースさせ、SyntaxErrorを実行時に発生させる
+    eval("foo! bar!");
+  } catch (error) {
+    console.log(error instanceof SyntaxError); // => true
+    console.log(error.name); // => "SyntaxError"
+    console.log(error.message); // => Unexpected token '!'
+  }
+}
+{
+  // ビルトインエラー(TypeError)
+  try {
+    // 関数ではないオブジェクトを関数として呼び出す
+    const fn = {};
+    fn();
+  } catch (error) {
+    console.log(error instanceof TypeError); // => true
+    console.log(error.name); // => "TypeError"
+    console.log(error.message); // エラーメッセージが表示される
+  }
+}
