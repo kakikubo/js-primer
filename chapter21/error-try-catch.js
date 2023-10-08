494b695bf6e8bd1f7f4dd27204832b21e4c8457e
kakikubo <kakikubo@gmail.com>
2023-10-08 16:14:36 +0900

    ビルトインエラーを投げる

--- chapter21/error-try-catch.js
+++ chapter21/error-try-catch.js
@@ -92,3 +92,23 @@
     console.log(error.message); // エラーメッセージが表示される
   }
 }
+
+// ビルトインエラーを投げる
+{
+  // 文字列を反転する関数
+  function reverseString(str) {
+    if (typeof str !== "string") {
+      throw new TypeError(`${str} is not a string`);
+    }
+    return Array.from(str).reverse().join("");
+  }
+
+  try {
+    // 数値を渡す
+    reverseString(100);
+  } catch (error) {
+    console.log(error instanceof TypeError); // => true
+    console.log(error.name); // => TypeError
+    console.log(error.message); // => "100 is not a string"
+  }
+}
