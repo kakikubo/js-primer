81fa5f8759487c43c1737f6877ca23a4d40a25d6
kakikubo <kakikubo@gmail.com>
2023-10-16 07:09:00 +0900

    fetchAPIを利用した通信
    * github のAPIから取得したユーザー情報のJSONオブジェクトをコンソールに出力した
    * Fetch APIの呼び出しに対するエラーハンドリングを行った
    * fetchUserInfo関数を宣言し、ボタンのクリックイベントで呼び出した

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,1 +1,20 @@
 console.log("index.js: loaded!");
+// const userId = "js-primer-example";
+
+function fetchUserInfo(userId) {
+  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then((response) => {
+      console.log(response.status);
+      // エラーレスポンスが返されたことを検知する
+      if (!response.ok) {
+        console.error("エラーレスポンス", response);
+      } else {
+        return response.json().then((userInfo) => {
+          console.log(userInfo);
+        });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
