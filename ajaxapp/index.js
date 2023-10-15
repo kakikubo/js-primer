04c3ecf96ef66b768973b189c27b0512d7c17306
kakikubo <kakikubo@gmail.com>
2023-10-16 07:41:16 +0900

    データを表示する
    * テンプレートリテラル(${userInfo.name}など)をつかってHTML文字列を組み立てた
    * innerHTMLプロパティを使ってHTML文字列をDOMに追加した
    * タグつきテンプレート関数(escapeHTML``)を使ってHTML文字列をエスケープした
    * fetchUserInfo関数を呼び出し、HTMLにユーザ情報が表示される事を確認した

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -11,10 +11,47 @@ function fetchUserInfo(userId) {
       } else {
         return response.json().then((userInfo) => {
           console.log(userInfo);
+          // HTMLの組み立て
+          const view = escapeHTML`
+          <h4>${userInfo.name} (@${userInfo.login})</h4>
+          <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+          <dl>
+            <dt>Location</dt>
+            <dd>${userInfo.location}</dd>
+            <dt>Repositories</dt>
+            <dd>${userInfo.public_repos}</dd>
+          </dl>
+          `;
+          // HTMLの挿入
+          const result = document.getElementById("result");
+          result.innerHTML = view;
         });
       }
     })
     .catch((error) => {
       console.error(error);
     });
+}
+
+function escapeSpecialChars(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+// See. https://github.com/kakikubo/js-primer/blob/develop/chapter15/string.js#L542-L557
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      console.log(`special value = ${value} str=${str}`);
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      console.log(`value = ${value} str=${str}`);
+      return result + String(value) + str;
+    }
+  });
 }
