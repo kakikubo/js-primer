86505961413e76d5310784ea01df745e76ea7843
kakikubo <kakikubo@gmail.com>
2023-10-17 07:25:18 +0900

    commanderパッケージをインストールして引数を簡単に扱えるように

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+// commanderモジュールからprogramオブジェクトをインポートする
+import { program } from "commander";
+
+// コマンドライン引数をcommanderでパースする
+program.parse(process.argv);
+
+// ファイルパスをprogram.args配列から取り出す
+const filePath = program.args[0];
+console.log(filePath);
