1b661c9761bd59dc1f3cfbe14abbf4716c0816a2
kakikubo <kakikubo@gmail.com>
2023-10-19 07:08:05 +0900

    エラーハンドリング

--- nodecli/main.js
+++ nodecli/main.js
@@ -12,6 +12,12 @@ const filePath = program.args[0];
 console.log(filePath);
 
 // ファイルを非同期で読み込む
-fs.readFile(filePath, { encoding: "utf8" }).then((file) => {
-  console.log(file);
-});
+fs.readFile(filePath, { encoding: "utf8" })
+  .then((file) => {
+    console.log(file);
+  })
+  .catch((err) => {
+    console.error(err.message);
+    // 終了ステータス1(一般的なエラー)としてプロセスを終了する
+    process.exit(1);
+  });
