22ac0d9e0c31b9fffd00c9baf8e0582bed372973
kakikubo <kakikubo@gmail.com>
2023-10-19 06:58:04 +0900

    sample.mdファイルを作成し、main.jsから読み込ませ、出力する

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,9 +1,17 @@
 // commanderモジュールからprogramオブジェクトをインポートする
 import { program } from "commander";
 
+// fs/promisesモジュールをfsオブジェクトとしてインポートする
+import * as fs from "node:fs/promises";
+
 // コマンドライン引数をcommanderでパースする
 program.parse(process.argv);
 
 // ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
 console.log(filePath);
+
+// ファイルを非同期で読み込む
+fs.readFile(filePath, { encoding: "utf8" }).then((file) => {
+  console.log(file);
+});
