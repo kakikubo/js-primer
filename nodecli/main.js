1f25e2acc8a09f73bfc8a0b3653226d1ba389f90
kakikubo <kakikubo@gmail.com>
2023-10-19 07:21:40 +0900

    markdown文字列をHTML文字列に変換
    * コマンドライン引数でmarkedの変換オプションを設定した
    * デフォルトオプションを定義し、コマンドライン引数で上書きできるようにした

--- nodecli/main.js
+++ nodecli/main.js
@@ -3,18 +3,34 @@ import { program } from "commander";
 
 // fs/promisesモジュールをfsオブジェクトとしてインポートする
 import * as fs from "node:fs/promises";
+// markedモジュールからmarkedオブジェクトをインポートする
+import { marked } from "marked";
 
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする");
 // コマンドライン引数をcommanderでパースする
 program.parse(process.argv);
+// オプションのパース結果をオブジェクトとして取得する
+const options = program.opts();
+console.log(options.gfm);
+// コマンドライン引数で指定されなかったオプションにデフォルト値を上書きする
+const cliOptions = {
+  gfm: options.gfm ?? false,
+};
 
 // ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
-console.log(filePath);
+// console.log(filePath);
 
 // ファイルを非同期で読み込む
 fs.readFile(filePath, { encoding: "utf8" })
   .then((file) => {
-    console.log(file);
+    // MarkdownファイルをHTML文字列に変換する
+    // gfmオプションを無効にする
+    const html = marked(file, {
+      gfm: cliOptions.gfm,
+    });
+    console.log(html);
   })
   .catch((err) => {
     console.error(err.message);
